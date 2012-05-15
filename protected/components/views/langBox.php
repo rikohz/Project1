19f9a590a03249a8f0ee58bf3870060993437b28
taylee <taylee@yahoo.cn>
2012-05-15 15:39:18 +0800

    Modify the parameter to lowercase.

--- protected/components/views/langBox.php
+++ protected/components/views/langBox.php
@@ -2,7 +2,7 @@
     $myFunctions = new MyFunctions()
 ?>
 
-<a href="<?php echo $myFunctions->ajouterParametreGET(Yii::app()->request->Url,'lang','FR_FR') ?>"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/flag-FR_FR.png" /></a>
-<a href="<?php echo $myFunctions->ajouterParametreGET(Yii::app()->request->Url,'lang','EN_US') ?>"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/flag-EN_US.png" /></a>
-<a href="<?php echo $myFunctions->ajouterParametreGET(Yii::app()->request->Url,'lang','ZH_CN') ?>"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/flag-ZH_CN.png" /></a>
+<a href="<?php echo $myFunctions->ajouterParametreGET(Yii::app()->request->Url,'lang','fr_fr') ?>"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/flag-FR_FR.png" /></a>
+<a href="<?php echo $myFunctions->ajouterParametreGET(Yii::app()->request->Url,'lang','en_us') ?>"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/flag-EN_US.png" /></a>
+<a href="<?php echo $myFunctions->ajouterParametreGET(Yii::app()->request->Url,'lang','zh_cn') ?>"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/flag-ZH_CN.png" /></a>
 
